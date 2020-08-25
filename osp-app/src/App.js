b6888cbfea51f18f4c84552a9755fc36128b5f60
KatarzynaZapala <64546867+KatarzynaZapala@users.noreply.github.com>
2020-08-25 14:44:55 +0200

    add nav details

--- osp-app/src/App.js
+++ osp-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.scss';
 import Nav from "./js/Nav";
 
@@ -8,22 +7,7 @@ import Nav from "./js/Nav";
 function App() {
   return (
       <Nav/>
-    // <div className="App">
-    //   <header className="App-header">
-    //     {/*<img src={logo} className="App-logo" alt="logo" />*/}
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+
   );
 }
 
--- osp-app/src/js/Nav.js
+++ osp-app/src/js/Nav.js
@@ -4,7 +4,7 @@ import React, {Component} from "react";
 export default class Nav extends Component {
     render() {
         return (
-        <header className="header">
+        <nav className="nav_container">
             <input className="menu_btn" type="checkbox" id="menu_btn"/>
             <label className="menu_icon" htmlFor="menu_btn"><span className="nav_icon"></span></label>
             <ul className="menu">
@@ -13,7 +13,7 @@ export default class Nav extends Component {
                 <li><a href="#rescuers">Ratownicy</a></li>
                 <li><a href="#contact">Kontakt</a></li>
             </ul>
-        </header>
+        </nav>
         )
     }
 }
